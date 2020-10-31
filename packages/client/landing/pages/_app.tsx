77d1a09f093e34b304c237551cc2882ae66c78f2
Mathijs Rutgers <MathijsR94@users.noreply.github.com>
2020-11-01 00:57:20 +0100

    feat(axe-core): add @axe-core/react voor accessibility audits (#53)
    * feat(axe-core): add @axe-core/react for accessibility audits during development

--- packages/client/landing/pages/_app.tsx
+++ packages/client/landing/pages/_app.tsx
@@ -3,6 +3,19 @@ import {AppProps} from 'next/app';
 import Nav from '../components/nav';
 import '../css/index.css';
 
+/**
+ * Not the prettiest solution, axe makes use of window variables, which cannot be handled by nextjs
+ * So, in dev mode we dynamically import the module and start if window is available
+ */
+if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
+    const promises = [import('@axe-core/react'), import('react-dom')];
+    Promise.all(promises).then((result) => {
+        const axe: any = result[0]?.default;
+        const ReactDOM = result[1]?.default;
+        if (axe && ReactDOM) axe(React, ReactDOM, 1000, {}, {exclude: [['#__next-prerender-indicator']]});
+    });
+}
+
 export default function App({Component, pageProps}: AppProps) {
     return (
         <div className="bg-gray-100 min-h-screen">
