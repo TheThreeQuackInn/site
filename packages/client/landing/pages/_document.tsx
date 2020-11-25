457956018e77fe99ea0433c4e23c40715a45fb52
Mathijs Rutgers <mathijsrutgers@gmail.com>
2020-11-25 21:30:38 +0100

    fix(darkmode): get initial theme on startup app

--- packages/client/landing/pages/_document.tsx
+++ packages/client/landing/pages/_document.tsx
@@ -1,5 +1,25 @@
 import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
 
+const setPreferredTheme = `
+    (function() {
+        function getThemePreference() {
+            if (typeof window === 'undefined') return null;
+            var themes = ['light', 'dark'];
+            var savedPreference = localStorage.getItem('theme');
+            if (savedPreference && themes.includes(savedPreference)) return savedPreference;
+
+            var darkModePreference = window.matchMedia('(prefers-color-scheme: dark)');
+            if (darkModePreference.media === 'not all') return 'light';
+            else if (darkModePreference.matches) return 'dark';
+            return 'light';
+        }
+
+        var colorMode = getThemePreference();
+        var root = document.documentElement;
+        root.setAttribute('class', colorMode);
+  })()
+`;
+
 export default class CustomDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
@@ -11,6 +31,7 @@ export default class CustomDocument extends Document {
             <Html lang="en">
                 <Head />
                 <body>
+                    <script dangerouslySetInnerHTML={{__html: setPreferredTheme}}></script>
                     <Main />
                     <NextScript />
                 </body>
