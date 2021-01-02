32a15027cde5a73c66771fbd5e6776cb7f9a1292
Mathijs Rutgers <mathijsrutgers@gmail.com>
2021-01-02 22:08:57 +0100

    feat(home): add little descriptive text

--- packages/client/landing/pages/index.tsx
+++ packages/client/landing/pages/index.tsx
@@ -7,6 +7,17 @@ export default function HomePage() {
         <Fragment>
             <AppHead title="The Three Quack Inn" />
             <Hero />
+            <div className="flex py-10 container mx-auto flex-col text-center">
+                <h2 className="tracking-tight leading-10 font-extrabold dark:text-white text-gray-900 xs:text-4xl sm:leading-none md:text-4xl mb-4">
+                    The Three Quack Inn
+                </h2>
+                <p className="sm:text-lg text-gray-900 dark:text-gray-300">
+                    Welcome to the Three Quack Inn, an inn dedicated to adventurers and storytellers alike. Come sit by
+                    the warm fires, eat until your belly is full and sleep like a king in ours beds. The Three Quack Inn
+                    helps Gamemasters and players with tools to use in their games. Only a few tools are available right
+                    now, but more features are on the list to be added.
+                </p>
+            </div>
         </Fragment>
     );
 }
