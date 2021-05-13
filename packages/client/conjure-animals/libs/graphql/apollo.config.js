bf85987950e1dd8af71f7da87578acf1dbb2aa1e
Mathijs Rutgers <mathijsrutgers@gmail.com>
2021-05-13 10:33:49 +0200

    fix(deps): update apollo and devdeps, update codegen conjure animals

--- packages/client/conjure-animals/libs/graphql/apollo.config.js
+++ packages/client/conjure-animals/libs/graphql/apollo.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 module.exports = {
     client: {
         addTypename: true,
-        includes: ['*.tsx'],
+        includes: [path.join(__dirname, '../../**/*.ts')],
         service: {
             name: '5e-srd-graphql',
             url: 'https://www.dnd5eapi.co/graphql',
